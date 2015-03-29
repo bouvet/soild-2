f839379c52554a0c8a836a6330ca6052b84a6193
toreerstad <tore.erstad@gmail.com>
2015-03-29 16:46:51 +0200

    delegation

---
+++ dotNet/delegation/delegation/CsvReader.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace delegation
+{
+    public class CsvReader
+    {
+        public void parse(string fileName)
+        {
+            Console.WriteLine("Starting to parse: " + fileName);
+            using (var reader = new StreamReader(fileName))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    string[] country = line.Split(',');
+                    Console.WriteLine("Country [code= " + country[4] + " , name=" + country[5] + "]");
+               }
+            }
+            Console.WriteLine("Done parsing: " + fileName);
+        }
+    }
+}
